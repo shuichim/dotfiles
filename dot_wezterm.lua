c9b4866ccd7c45a60a1b50cf9df3bc4bb6c6c1d4
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-09-19 08:53:37 -0500

    update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -9,7 +9,14 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = 'AdventureTime'
 
+-- and font:
+config.font = wezterm.font { family = 'HackGen Console NF', harfbuzz_features = { 'zero', 'ss01', 'cv05' } }
+config.font_size = 14.0
+
 config.window_background_opacity = 0.8
+
+-- Spawn a fish shell in login mode
+config.default_prog = { '/opt/homebrew/bin/fish', '-l' }
 
 -- and finally, return the configuration to wezterm
 return config
