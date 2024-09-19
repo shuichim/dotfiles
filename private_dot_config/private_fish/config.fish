0ca3df6c49183d3b7fb2899cc0eff34a762b5921
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-09-19 12:44:00 -0500

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,4 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     eval "$(/opt/homebrew/bin/brew shellenv)"
     starship init fish | source
+
+    if set -q ZELLIJ
+    else
+        zellij -l welcome
+    end
 end
