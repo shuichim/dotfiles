a64197e724d126f471cc3ea5d0c9423e99fbda34
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-09-20 13:28:10 -0500

    update abbreviations.fish

--- private_dot_config/private_fish/conf.d/empty_abbreviations.fish
+++ private_dot_config/private_fish/conf.d/empty_abbreviations.fish
@@ -7,14 +7,7 @@ abbr -a gco 'git checkout'
 # Basic replacements
 abbr -a l 'eza'
 abbr -a ls 'eza'
-abbr -a ll 'eza -l'
-abbr -a la 'eza -la'
-
-# Long format with various sorting options
-abbr -a llm 'eza -l --sort=modified'
-abbr -a lls 'eza -l --sort=size'
-abbr -a lla 'eza -l --sort=accessed'
-abbr -a llc 'eza -l --sort=created'
+abbr -a ll 'eza --long --all --header --icons --git'
 
 # Tree view
 abbr -a lt 'eza --tree'
@@ -24,27 +17,9 @@ abbr -a ltt 'eza --tree --level=2'
 abbr -a lsd 'eza -D'
 abbr -a lld 'eza -lD'
 
-# Git integration
-abbr -a lg 'eza -l --git'
-abbr -a lga 'eza -la --git'
-
-# Icons and colors
-abbr -a lic 'eza --icons --color=always'
-abbr -a llic 'eza -l --icons --color=always'
-
-# Grid and across
-abbr -a lsg 'eza --grid'
-abbr -a lsx 'eza --across'
-
-# Extended attributes
-abbr -a lx 'eza -l --extended'
-
 # Showing hidden files (dotfiles)
 abbr -a l. 'eza -d .*'
-
-# Recursive listing
-abbr -a lr 'eza -R'
-abbr -a lrr 'eza -lR'
+abbr -a ll. 'eza -ld .*'
 
 # Chezmoi related 
 abbr -a chad 'chezmoi add'
