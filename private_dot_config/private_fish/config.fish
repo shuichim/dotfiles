3956df7034395da2677e6bde66561215a4f85f8b
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-09-21 08:10:22 -0500

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    set -x EZA_COLORS "uu=36:un=0:gu=36:gn=0:da=90:sn=32:sb=32"
+    set -x EZA_COLORS "da=90"
 
     eval "$(/opt/homebrew/bin/brew shellenv)"
     starship init fish | source
