6bbbb5482df9a69a0c1f4e9e2dc6fa4d80fa0f4a
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-09-21 23:28:26 -0500

    Update .config/nvim/lua/plugins/obsidian.lua

--- private_dot_config/nvim/lua/plugins/obsidian.lua
+++ private_dot_config/nvim/lua/plugins/obsidian.lua
@@ -21,6 +21,24 @@ return {
 	  },
 	 notes_subdir = "00 inbox",
          new_notes_location = "notes_subdir",
+	 -- Optional, customize how note IDs are generated given an optional title.
+ 	 ---@param title string|?
+  	 ---@return string
+  	 note_id_func = function(title)
+	    local suffix = ""
+	    local currentTime = os.time()
+	    local formattedDate = os.date("%Y-%m-%d", currentTime)
+	    if title ~= nil then
+	      -- If title is given, transform it into valid file name.
+	      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+	    else
+	      -- If title is nil, just add 4 random uppercase letters to the suffix.
+	      for _ = 1, 4 do
+	        suffix = suffix .. string.char(math.random(65, 90))
+	      end
+	    end
+	    return formattedDate .. "-" .. suffix
+	 end,
          preferred_link_style = "markdown",
 	 use_advanced_uri = true,
 	 open_app_foreground = true,
@@ -29,4 +47,5 @@ return {
 	 }
 	},},
 	{ "nvim-lua/plenary.nvim", lazy = true },
+	{"hrsh7th/nvim-cmp", lazy = true},
 }
