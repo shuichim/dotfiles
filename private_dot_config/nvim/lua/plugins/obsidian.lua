20fa7ac9a02b5cd9c1aff6ca42888882635176bf
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-09-21 23:37:10 -0500

    Update .config/nvim/lua/plugins/obsidian.lua

--- private_dot_config/nvim/lua/plugins/obsidian.lua
+++ private_dot_config/nvim/lua/plugins/obsidian.lua
@@ -30,14 +30,14 @@ return {
 	    local formattedDate = os.date("%Y-%m-%d", currentTime)
 	    if title ~= nil then
 	      -- If title is given, transform it into valid file name.
-	      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+	      suffix = title:gsub("[^A-Za-z0-9-]", ""):lower()
 	    else
 	      -- If title is nil, just add 4 random uppercase letters to the suffix.
 	      for _ = 1, 4 do
 	        suffix = suffix .. string.char(math.random(65, 90))
 	      end
 	    end
-	    return formattedDate .. "-" .. suffix
+	    return formattedDate .. " " .. suffix
 	 end,
          preferred_link_style = "markdown",
 	 use_advanced_uri = true,
