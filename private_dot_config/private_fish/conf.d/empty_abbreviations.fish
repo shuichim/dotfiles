0b62b610e1c632ae1d4a962c7ff982491c558bc3
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-09-26 15:15:02 -0500

    Update .config/fish/conf.d/abbreviations.fish

--- private_dot_config/private_fish/conf.d/empty_abbreviations.fish
+++ private_dot_config/private_fish/conf.d/empty_abbreviations.fish
@@ -1,31 +1,33 @@
 if status is-interactive
 
-# git related
-abbr -a gcb 'git checkout -b' 
-abbr -a gco 'git checkout' 
+    # git related
+    abbr -a gcb 'git checkout -b'
+    abbr -a gco 'git checkout'
 
-# Basic replacements
-abbr -a l 'eza'
-abbr -a ls 'eza'
-abbr -a ll 'eza --long --all --header --icons --git'
+    # Basic replacements
+    abbr -a l eza
+    abbr -a ls eza
+    abbr -a ll 'eza --long --all --header --icons --git'
 
-# Tree view
-abbr -a lt 'eza --tree'
-abbr -a ltt 'eza --tree --level=2'
+    # Tree view
+    abbr -a lt 'eza --tree'
+    abbr -a ltt 'eza --tree --level=2'
 
-# Show only directories
-abbr -a lsd 'eza -D'
-abbr -a lld 'eza -lD'
+    # Show only directories
+    abbr -a lsd 'eza -D'
+    abbr -a lld 'eza -lD'
 
-# Showing hidden files (dotfiles)
-abbr -a l. 'eza -d .*'
-abbr -a ll. 'eza -ld .*'
+    # Showing hidden files (dotfiles)
+    abbr -a l. 'eza -d .*'
+    abbr -a ll. 'eza -ld .*'
 
-# Chezmoi related 
-abbr -a chad 'chezmoi add'
-abbr -a ched 'chezmoi edit'
-abbr -a chcd 'chezmoi cd'
-abbr -a chdiff 'chezmoi diff'
-abbr -a chap 'chezmoi apply'
+    # Chezmoi related 
+    abbr -a chad 'chezmoi add'
+    abbr -a ched 'chezmoi edit'
+    abbr -a chcd 'chezmoi cd'
+    abbr -a chdiff 'chezmoi diff'
+    abbr -a chap 'chezmoi apply'
+
+    abbr -a cat bat
 
 end
