59dbca8ba6264c3d17db7ba21dda96cd0ffeecd2
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-10-15 13:45:41 -0500

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,3 +1,4 @@
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x EZA_COLORS "da=90"
@@ -5,7 +6,8 @@ if status is-interactive
     eval "$(/opt/homebrew/bin/brew shellenv)"
     starship init fish | source
 
-    if set -q ZELLIJ
+    if set -q ZELLIJ || string match -q "$TERM_PROGRAM" vscode
+
     else
         zellij -l welcome
     end
