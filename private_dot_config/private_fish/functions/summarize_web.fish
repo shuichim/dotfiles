53d0ab0a6b18e03353b2e6b414324439ffee6fe4
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-10-16 12:20:31 -0500

    Update .config/fish/functions/summarize_web.fish

--- private_dot_config/private_fish/functions/summarize_web.fish
+++ private_dot_config/private_fish/functions/summarize_web.fish
@@ -1,5 +1,5 @@
 function summarize_web
     source ~/python_script/agent/venv/bin/activate.fish
-    python3 ~/python_script/agent/web_summary_agent.py $argv
+    python3 ~/python_script/agent/web_summary_agent.py $argv | tee /dev/tty | pbcopy
     deactivate
 end
