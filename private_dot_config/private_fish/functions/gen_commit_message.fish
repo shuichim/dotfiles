625359999a47f6b8d6b1dbd6d2f905cf70611dc9
Shuichi Maruyama <smaruyam@hawk.iit.edu>
2024-10-16 12:20:56 -0500

    Update .config/fish/functions/gen_commit_message.fish

--- private_dot_config/private_fish/functions/gen_commit_message.fish
+++ private_dot_config/private_fish/functions/gen_commit_message.fish
@@ -1,5 +1,5 @@
 function gen_commit_message
     source ~/python_script/agent/venv/bin/activate.fish
-    python3 ~/python_script/agent/commit_message_generator.py $argv
+    python3 ~/python_script/agent/commit_message_generator.py $argv | tee /dev/tty | pbcopy
     deactivate
 end
